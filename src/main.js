d712038de27db0a4cb9fbfc715a3f65377285c33
Hugo <hugovdbovenkamp@gmail.com>
2024-12-02 15:08:48 +0100

    filtered products and shows the products by category when clicking the <li>

--- src/main.js
+++ src/main.js
@@ -9,20 +9,3 @@ function animateMenu() {
     menuOpenButton.classList.toggle("hidden");
     menuCloseButton.classList.toggle("hidden");
 }
-
-// Get json data from local json file and log fetched data
-fetch('http://localhost:63342/dutch-tackle/products.json')
-.then(response=> {
-    if(!response.ok) {
-        throw new Error('HTTP Error! status:' . response.status);
-    }
-    console.log(response);
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
-
